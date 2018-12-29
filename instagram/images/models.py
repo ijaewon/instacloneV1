1a83ccb2ea3036857c19017da16754afc181c173
이재원 <jwlee@ijaewon-ui-MacBook-Pro.local>
2018-12-30 05:25:11 +0900

    Doing it again...

---
+++ instagram/images/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from instagram.users import models as user_models
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Image(TimeStampedModel):
+    """ Image Model"""
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True, related_name="images")
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+class Comment(TimeStampedModel):
+
+    """Comment Model"""
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True, related_name="comments")
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.message
+
+class Like(TimeStampedModel):
+
+    """Like Model"""
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True, related_name="likes")
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return 'User : {} - Image Caption : {}'.format(self.creator.username, self.image.caption)
