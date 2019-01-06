9a7834805ff42103fb2153d105fc6b7178362659
이재원 <jwlee@ijaewon-ui-MacBook-Pro.local>
2019-01-06 19:10:02 +0900

    Serializing the Tags

--- instagram/images/serializers.py
+++ instagram/images/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from . import models
+from taggit_serializer.serializers import (TagListSerializerField,
+                                           TaggitSerializer)
 from instagram.users import models as user_models
 
 class SmallImageSerializer(serializers.ModelSerializer):
@@ -52,10 +54,11 @@ class LikeSerializer(serializers.ModelSerializer):
 
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageSerializer(TaggitSerializer, serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
     creator = FeedUserSerializer()
+    tags = TagListSerializerField()
 
     class Meta:
         model=models.Image
@@ -67,6 +70,7 @@ class ImageSerializer(serializers.ModelSerializer):
             'comments',
             'like_count',
             'creator',
+            'tags',
             'created_at'
         )
 
