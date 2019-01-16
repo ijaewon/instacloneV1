88308a22abd387ab246f693c84529c85941b9c0c
이재원 <jwlee@ijaewon-ui-MacBook-Pro.local>
2019-01-17 03:40:17 +0900

    Auth Components

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
-import styles from './styles.scss';
+import React from 'react';
+import  propTypes from 'prop-types';
+import { Route, Switch } from 'react-router-dom';
+import './styles.scss';
 import Footer from 'components/Footer';
+import Auth from 'components/Auth';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer/>
-      </div>
-    );
-  }
+const App = props => [
+    // Nav.
+    props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+    <Footer key={3}/>
+];
+
+App.propTypes = {
+    isLoggedIn: propTypes.bool.isRequired,
 }
+
+const PrivateRoutes = props => (
+    <Switch>
+        <Route exact path="/" render={() => "feed"}/>
+        <Route exact path="/explore" render={() => "explore"}/>
+    </Switch>
+);
+
+const PublicRoutes = props => (
+    <Switch>
+        <Route exact path="/" component={Auth}/>
+        <Route exact path="/forgot" render={() => "password"}/>
+    </Switch>
+);
 
 export default App;
---
+++ frontend/src/components/Auth/presenter.js
@@ -0,0 +1,56 @@
+
+import React from "react";
+import "./styles.scss";
+import { LoginForm, SignupForm } from 'components/AuthForms';
+
+const Auth = (props, context) => (
+  <main className="auth">
+    {/* <div className="column">
+      <img src={require("images/phone.png")} alt="Checkout our app. Is cool" />
+    </div> */}
+    <div className="column">
+        <div className="white-box">
+            {props.action === 'login' && <LoginForm/>}
+            {props.action === 'signup' && <SignupForm/>}
+        </div>
+      <div className="white-box">
+        {props.action === 'login' && (
+            <p>
+            Don't have an account?{" "}
+            <span
+              className="change-link"
+              onClick={props.changeAction}
+            >
+              Sign up
+            </span>
+          </p>
+        )}
+        {props.action === 'signup' && (
+            <p>
+            Have an account?{" "}
+            <span
+              className="change-link"
+              onClick={props.changeAction}
+            >
+              Log in
+            </span>
+          </p>
+        )}
+      </div>
+      <div className="app-box">
+        <span>Get the app</span>
+        <div className="appstores">
+          <img
+            src={require("images/ios.png")}
+            alt="Download it on the Apple Appstore"
+          />
+          <img
+            src={require("images/android.png")}
+            alt="Download it on the Apple Appstore"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
+);
+export default Auth;
