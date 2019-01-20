6b811970e8c304170099b1b9f7f5cb5dea5303e7
이재원 <jwlee@ijaewon-ui-MacBook-Pro.local>
2019-01-21 05:25:34 +0900

    Importing Translations

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./styles.scss";
+import propTypes from 'prop-types';
 
 const Footer = (props, context) => (
   <footer className="footer">
     <div className="column">
       <nav className="nav">
         <ul className="list">
-          <li className="list-item">About Us</li>
-          <li className="list-item">Support</li>
-          <li className="list-item">Blog</li>
-          <li className="list-item">Press</li>
-          <li className="list-item">API</li>
-          <li className="list-item">Jobs</li>
-          <li className="list-item">Privacy</li>
-          <li className="list-item">Terms</li>
-          <li className="list-item">Directory</li>
-          <li className="list-item">Language</li>
+          <li className="list-item">{context.t("About Us")}</li>
+          <li className="list-item">{context.t("Support")}</li>
+          <li className="list-item">{context.t("Blog")}</li>
+          <li className="list-item">{context.t("Press")}</li>
+          <li className="list-item">{context.t("API")}</li>
+          <li className="list-item">{context.t("Jobs")}</li>
+          <li className="list-item">{context.t("Privacy")}</li>
+          <li className="list-item">{context.t("Terms")}</li>
+          <li className="list-item">{context.t("Directory")}</li>
+          <li className="list-item">{context.t("Language")}</li>
         </ul>
       </nav>
     </div>
@@ -24,6 +25,11 @@ const Footer = (props, context) => (
     </div>
   </footer>
 );
+
+Footer.contextTypes = {
+  t: propTypes.func.isRequired
+};
+
 
 export default Footer;
 
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,16 @@
 export const translations = {
-    es: {
-        "Login in": "Iniciar Session"
-    }
+  'kr': {
+    'About Us': 'INSTAGRAM정보',
+    'Support': '지원',
+    'Blog': '블로그',
+    'Press': '프레스',
+    'API': 'API',
+    'Jobs': '채용 정보',
+    'Privacy': '개인정보처리방침',
+    'Terms': '약관',
+    'Directory': '디렉터리',
+    'Language': '언어',
+  },
+  'options': {
+  },
 }
