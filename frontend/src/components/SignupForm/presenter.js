2ba1df776e944a2e98f0872dc86df99b4fbde4c8
이재원 <jwlee@ijaewon-ui-MacBook-Pro.local>
2019-01-21 09:08:39 +0900

    Sign Up Form Coding Challenge Solution

--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Ionicon from "react-ionicons";
+import propTypes from 'prop-types';
 import '../shared/formStyles.scss';
 
 
@@ -13,25 +14,54 @@ const SignupForm = props => (
         Facebook
       </button>
       <span className="divider">or</span>
-      <form className="form">
-        <input type="email" placeholder="Email" className="text-input" />
-        <input type="text" placeholder="Full Name" className="text-input" />
+      <form className="form" onSubmit={props.handleSubmit} >
+        <input 
+          type="email" 
+          placeholder="Email" 
+          className="text-input"
+          value={props.emailValue}
+          onChange={props.handleInputChange}
+          name="email"
+        />
+        <input 
+          type="text" 
+          placeholder="Full Name" 
+          className="text-input" 
+          value={props.fullNameValue}
+          onChange={props.handleInputChange}
+          name="fullName"
+        />
         <input
           type="username"
           placeholder="Username"
           className="text-input"
+          value={props.usernameValue}
+          onChange={props.handleInputChange}
+          name="username"
         />
         <input
           type="password"
           placeholder="Password"
           className="text-input"
+          value={props.passwordValue}
+          onChange={props.handleInputChange}
+          name="password"
         />
-        <input type="submit" value="Sign up" className="button" />
+        <input type="submit" value="Sign up" className="button" onChange={props.handleInputChange}/>
       </form>
       <p className="terms">
         By signing up, you agree to our <span>Terms & Privacy Policy</span>.
       </p>
     </div>
   );
+
+  SignupForm.propTypes = {
+    emailValue: propTypes.string.isRequired,
+    fullNameValue: propTypes.string.isRequired,
+    usernameValue: propTypes.string.isRequired,
+    passwordValue: propTypes.string.isRequired,
+    handleInputChange: propTypes.func.isRequired,
+    handleSubmit: propTypes.func.isRequired,
+  }
 
 export default SignupForm;
